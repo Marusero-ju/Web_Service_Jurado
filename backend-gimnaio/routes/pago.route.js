ddfca2e17adf77036f61a4e47388575ddb6d9777
Marusero-ju <65613970+Marusero-ju@users.noreply.github.com>
2021-06-21 17:27:02 -0300

    Create pago.route.js

---
+++ backend-gimnaio/routes/pago.route.js
@@ -0,0 +1,17 @@
+//defino controlador para el manejo de CRUD
+const pagoCtrl = require('./../controllers/pago.controller');
+
+//creamos el manejador de rutas
+const express = require('express');
+const router = express.Router();
+
+//definimos las rutas para la gestion de agente
+router.get('/', pagoCtrl.getPago);
+router.post('/', pagoCtrl.createPago);
+router.get('/:id', pagoCtrl.getPago);
+router.put('/:id', pagoCtrl.editPago);
+router.delete('/:id', pagoCtrl.deletePago);
+
+
+//exportamos el modulo de rutas
+module.exports = router;
