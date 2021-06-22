29eb098a3d51a0a0c8ec4b8997bbff3773e6597c
matimix <neo.franco1@gmail.com>
2021-06-22 18:31:46 -0300

    creacion de la ruta de entrenador

--- backend-gimnaio/routes/alumno.route.js
+++ backend-gimnaio/routes/alumno.route.js
@@ -3,7 +3,7 @@ const alumnoCtrl = require('./../controllers/alumno.controller');
 //creamos el manejador de rutas
 const express = require('express');
 const router = express.Router();
-//definimos las rutas para la gestion de agente
+//definimos las rutas para la gestion de alumno
 router.get('/', alumnoCtrl.getAlumno);
 router.post('/', alumnoCtrl.createAlumno);
 router.get('/:id', alumnoCtrl.getAlumno);
