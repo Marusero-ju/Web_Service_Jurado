6178fb05bb01efadf7f39b4c42c3cf82e8a87f4f
matimix <neo.franco1@gmail.com>
2021-06-24 02:56:59 -0300

    se agrega al index la ruta de usuario

--- backend-gimnaio/index.js
+++ backend-gimnaio/index.js
@@ -12,6 +12,7 @@ app.use('/api/agente', require('./routes/agente.route.js'));
 app.use('/api/sector', require('./routes/sector.route.js'));
 app.use('/api/alumno', require('./routes/alumno.route.js'));
 app.use('/api/plan', require('./routes/planes.route.js'));
+app.use('/api/usuarios', require('./routes/usuario.route'));
 
 //setting
 app.set('port', process.env.PORT || 3000);
