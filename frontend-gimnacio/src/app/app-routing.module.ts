047f2171d69e2c0a6e54b58c772e4b8988c75152
matimix <neo.franco1@gmail.com>
2021-06-24 04:09:09 -0300

    creacion de la vista y ruteo del login

--- frontend-gimnacio/src/app/app-routing.module.ts
+++ frontend-gimnacio/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AlumnoFormComponent } from './components/alumno/alumno-form/alumno-form
 import { HomeComponent } from './components/layout/home/home.component';
 import { PagoFormComponent } from './components/pago/pago-form/pago-form.component';
 import { PagoComponent } from './components/pago/pago.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'alumno-form', component: AlumnoFormComponent},
   {path: 'pago-table', component: PagoComponent},
-  {path: 'pago-form', component: PagoFormComponent}
+  {path: 'pago-form', component: PagoFormComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
