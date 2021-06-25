36c1378a83a50d4045acd4877f1543eb147e6e50
ematala015 <ema.tala015@gmail.com>
2021-06-25 18:41:09 -0300

    Cambios raros

--- frontend-gimnacio/src/app/app.module.ts
+++ frontend-gimnacio/src/app/app.module.ts
@@ -18,6 +18,9 @@ import { PagoFormComponent } from './components/pago/pago-form/pago-form.compone
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NgxDataTableModule } from 'angular-9-datatable';
+import { LoginService } from './services/login.service';
+import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -31,7 +34,8 @@ import { NgxDataTableModule } from 'angular-9-datatable';
     MenuComponent,
     NavbarComponent,
     PagoComponent,
-    PagoFormComponent
+    PagoFormComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -42,7 +46,7 @@ import { NgxDataTableModule } from 'angular-9-datatable';
     FormsModule,
     NgxDataTableModule
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
