6e3c16d193967d7dac272df383dfa6d6157ea85f
ematala015 <ema.tala015@gmail.com>
2021-06-29 01:18:06 -0300

    Se agrego en el backend el servicio para subir imagenes desde el front a la página web

--- backend-gimnaio/models/cardEntrenamiento.js
+++ backend-gimnaio/models/cardEntrenamiento.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const path = require('path');
 const {Schema} = mongoose;
 
 const CardSchema = new Schema({
@@ -6,5 +7,14 @@ const CardSchema = new Schema({
     descripcion: {type: String, required: true},
     img: {type: String, required: true},
 });
+
+//Methods 
+
+CardSchema.methods.setImgUrl = function setImgUrl(filename){
+    this.img = `http://localhost:3000/image/${filename}`;
+    // this.img = path.join(__dirname, 'storage/images/'+ `${filename}`)
+}
+
+
 //exporto objeto para que pueda ser usado en otros lugares
 module.exports = mongoose.model('Card', CardSchema);
